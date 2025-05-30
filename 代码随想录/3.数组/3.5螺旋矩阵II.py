3d350da87fd8c82b920ac4501ff276a5c5c70507
dpsfigo <dps_figo@hotmail.com>
2025-05-30 18:00:29 +0800

    feat:code daily

---
+++ 代码随想录/3.数组/3.5螺旋矩阵II.py
@@ -0,0 +1,19 @@
+"""
+Author: dpsfigo
+Date: 2025-05-30 16:47:19
+LastEditors: dpsfigo
+LastEditTime: 2025-05-30 16:47:59
+Description: 请填写简介
+"""
+
+"""
+https://leetcode.cn/problems/spiral-matrix-ii/
+给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+示例:
+输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
+"""
+
+class Solution():
+    def spiral_matrix_ii(n:int)->list:
+        start_x, start_y = 0, 0
+        loop =  
